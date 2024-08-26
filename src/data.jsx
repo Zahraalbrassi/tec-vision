ba88829f10d9c609068a27b17b472f0c89a12361
zahraalbrassi <Zahraalbrassi@gmail.com>
2024-08-26 21:49:04 +0200

    the last changes

--- src/data.jsx
+++ src/data.jsx
@@ -4,7 +4,7 @@ import Lighting_Projectors from './assets/Solar Lighting Projectors1.jpg';
 import Security_cameras from './assets/Security cameras.jpg';
 import Batteries from './assets/Batteries.jpg';
 import Interferometers from './assets/Interferometers.jpg';
-import Solar from './assets/Solar_img4.jpg';
+import Solar from './assets/Solar_img.jpg';
 
 
 export const solarPowerData = [
@@ -31,12 +31,12 @@ export const solarPowerData = [
     {
         imageSource: Batteries,
         title: 'Batteries',
-        description: 'We provide the most important component in any solar system and the batteries we offer in different sizes ranging from 40-210 amps / hour .. It is the most efficient batteries that operate under any circumstances and the cycle of discharge at high levels.'
+        description: 'We provide the most important component in any solar system, It is the most efficient batteries that operate under any circumstances and the cycle of discharge at high levels.'
     },
     {
         imageSource: Interferometers,
         title: 'Interferometers',
-        description: 'We provide many types of inverters such as: grid-connected inverter, inverter, separated by grid and hybrid, featuring: 80-amp solar and electric chargers, providing protection from high voltage and low voltage currents, Household, contains a current stabilizer to maintain the stability of the network and work on the process of charging and generation and save the current automatically..'
+        description: 'We provide many types of inverters such as: grid-connected inverter, separated by grid and hybrid, contains a current stabilizer to maintain the stability of the network and save the current automatically.'
     },
     {
         imageSource: Solar,
